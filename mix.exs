9fcd596ae4718312e71e43ea157cf11338581f39
Igor Šarčević <igor@renderedtext.com>
2016-08-21 21:48:35 +0200

    Add plug anv tackle deps

--- mix.exs
+++ mix.exs
@@ -10,23 +10,20 @@ defmodule HttpTackle.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [
+      :plug,
+      :cowboy,
+      :logger,
+      :tackle
+    ]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+     {:tackle, git: "git@github.com:renderedtext/ex-tackle", red: "origin/master"},
+     {:cowboy, "~> 1.0"},
+     {:plug, "~> 1.0"},
+    ]
   end
 end
